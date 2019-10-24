7a8103c689cb34e4e0e8e4eb41e2c38b57b9cca8
Hunter <haeilmes@gmail.com>
2019-10-24 12:32:57 -0500

    final

--- weeklyChallenges07/ChallengesSet07.cs
+++ weeklyChallenges07/ChallengesSet07.cs
@@ -1,38 +1,122 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace weeklyChallenges07
 {
     public class ChallengesSet07
     {
-            
+
         public int CountOfBusinessesWithNegativeNetProfit(List<Business> businesses)
         {
-            throw new NotImplementedException();
+            int num = 0;
+
+            if (businesses == null)
+                return 0;
+
+            foreach (Business business in businesses)
+            {
+                if (business.TotalRevenue < business.TotalExpenses)
+                    num++;
+            }
+
+            return num;
         }
 
         public string GetCommaSeparatedListOfProfitableBusinesses(List<Business> businesses)
         {
-            throw new NotImplementedException();
+            if (businesses == null)
+                return "";
+
+            string profitableBuisnesses = "";
+
+            foreach (Business business in businesses)
+            {
+                if (business.TotalRevenue > business.TotalExpenses)
+                {
+                    profitableBuisnesses += $"{business.Name},";
+                }
+            }
+
+            return profitableBuisnesses.Trim(',');
         }
 
         public string GetNameOfHighestParentCompany(Business business)
         {
-            // If there is Company A, whose parent is Company B, whose parent is Company C, then given Company A return Company C
-            throw new NotImplementedException();
+            string parent = business.Name;
+
+            while (business.ParentCompany != null)
+            {
+                parent = business.ParentCompany.Name;
+                business = business.ParentCompany;
+            }
+
+            return parent;
         }
 
         public enum TicTacToeResult { X, O, Draw }
+
         public TicTacToeResult GetTicTacToeWinner(char[,] finalBoard)
         {
-            throw new NotImplementedException();
+            if (checkWinner(finalBoard[0, 0], finalBoard[1, 1], finalBoard[2, 2]) || checkWinner(finalBoard[0, 2], finalBoard[1, 1], finalBoard[2, 0]))
+            {
+                return returnWinner(finalBoard[1, 1]);
+            }
+
+            for (int i = 0; i < finalBoard.GetLength(0); i++)
+            {
+                if (checkWinner(finalBoard[i, 0], finalBoard[i, 1], finalBoard[i, 2]))
+                {
+                    return returnWinner(finalBoard[i, 0]);
+                }
+
+                else if (checkWinner(finalBoard[0, i], finalBoard[1, i], finalBoard[2, i]))
+                {
+                    return returnWinner(finalBoard[0, i]);
+                }
+            }
+
+            return TicTacToeResult.Draw;
+        }
+
+        private bool checkWinner(char a, char b, char c)
+        {
+            return (a == b && b == c);
+        }
+
+        private TicTacToeResult returnWinner(char c)
+        {
+            if (c == 'X')
+            {
+                return TicTacToeResult.X;
+            }
+
+            else
+            {
+                return TicTacToeResult.O;
+            }
         }
 
         public bool EachArrayInJaggedArrayContainsTargetNumber(int[][] numbers, int targetNumber)
         {
-            throw new NotImplementedException();
+            if (numbers == null || numbers.Length == 0)
+            {
+                return false;
+            }
+
+            bool hasTarget = true;
+
+            foreach (int[] nums in numbers)
+            {
+                if (!nums.Contains(targetNumber))
+                {
+                    hasTarget = false;
+                }
+            }
+            return hasTarget;
         }
+
     }
+
 }
 
- 
